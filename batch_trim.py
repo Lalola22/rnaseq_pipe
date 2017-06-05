cecf80af0fbc23f44c504811bc2f57c4a9270e89
Samuel Lee <samleenz@me.com>
2017-06-05 13:14:20 +1200

    tidy code indents

--- batch_trim.py
+++ batch_trim.py
@@ -67,12 +67,12 @@ def call_trimmomatic_par(subdir):
                 basename = dividing[0].replace("_R1", "")
                 # This is the forward read sans extensions
                 call("java -jar " + trim +
-                    "trimmomatic-0.36.jar PE -phred33 " + fulldir + fileR1 +
-                    " " + fulldir + fileR2 + " -baseout " + processed +
-                    basename + ".fastq.gz ILLUMINACLIP:" + aux +
-                    "TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 " +
-                    "SLIDINGWINDOW:4:15 MINLEN:35",
-                    shell=True)
+                     "trimmomatic-0.36.jar PE -phred33 " + fulldir + fileR1 +
+                     " " + fulldir + fileR2 + " -baseout " + processed +
+                     basename + ".fastq.gz ILLUMINACLIP:" + aux +
+                     "TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 " +
+                     "SLIDINGWINDOW:4:15 MINLEN:35",
+                     shell=True)
 
 
 # --- __main__ call
