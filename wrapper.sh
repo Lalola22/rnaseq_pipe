7f376d6aba19de515d767604786046105c09bb32
Samuel Lee <samleenz@me.com>
2017-06-10 10:22:16 +1200

    removed hard-coding of directory for deseq2_analysis.R

--- wrapper.sh
+++ wrapper.sh
@@ -156,11 +156,10 @@ Rscript create_tx2g.R "${outDir}/reference_files" "$cores"
 
 echo "DeSeq2 analysis of salmon quants happening..."
 
-echo "the quant dir is still hard-coded due to not running the full pipeline"
 
 for ((i = 1; i <= max_index; i++)); do
   echo "Running deseq2 for: " "${samples[i]}"
-  Rscript dseq2_analysis.R "/home/slee/outputs/test/june_09" \
+  Rscript dseq2_analysis.R "/$outDir" \
   "salmon" "$cores" ${samples[i]};
  done
 
