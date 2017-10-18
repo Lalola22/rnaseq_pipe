6af7d98ddbb49d849b18d873a3784e91b5c6fd25
Samuel Lee <samleenz@me.com>
2017-10-18 15:45:09 +1300

    initial add of support for mice / rat genes

--- create_tx2g.R
+++ create_tx2g.R
@@ -1,12 +1,16 @@
 # 09/06/2017
 # Sam Lee
 # Creation of the tx 2 gene table to DESeq2 mappings
-
+# Two arguments required
+# 1. dir to save map file to
+# 2. species to make the map for, there are three options
+#       a. Human
+#       b. Mouse
+#       c. Rat
 
 # Setup -------------------------------------------------------------------
 
 library(tidyverse, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
-library(BiocParallel, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
 library(ensembldb, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
 library(EnsDb.Hsapiens.v86, quietly = TRUE,
     verbose = FALSE, warn.conflicts = FALSE)
@@ -14,9 +18,24 @@ library(EnsDb.Hsapiens.v86, quietly = TRUE,
 args <- commandArgs(trailingOnly = TRUE)
 
 outdir <- args[1]
-cores <- args[2]
+species <- toLowere(args[2])
 
-register(MulticoreParam(cores))
+# check that the species is correct
+speciesCheck <- species %in% c("human", "mouse", "rat")
+if (! dircheck == TRUE){
+  stop('Species must be one of "human", "mouse", or "rat"')
+}
+
+# assign species to  correct Db
+txdb <- if (species == "human") {
+   library(EnsDb.Hsapiens.v86, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+} else {if (species == "mouse") {
+   library(EnsDb.Mmusculus.v79, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+} else {
+   library(EnsDb.Rnorvegicus.v79, quietly = TRUE, verbose = FALSE, warn.conflicts = FALSE)
+}
+   
+}
 
 # Code body -------------------------------------------------------------------
 
@@ -27,6 +46,6 @@ k <- keys(txdb, keytype = "GENENAME")
 df <- select(txdb, keys = k, keytype = "GENENAME", columns = "TXID")
 tx2gene <- df[, 2:1]  # tx ID, then gene ID
 
-print("Saving the gene to transcript mapping...", quote = FALSE)
+print(sprintf("Saving the gene to transcript mapping for %s", species), quote = FALSE)
 
 write_csv(tx2gene, path = file.path(outdir, "tx2g_table.csv"))
